af4b64eee6f45d111fada9caf0bdb345df6a114a
Vasyl Pryimak <vasyl.pryimak.vasyl.dev@gmail.com>
2026-03-29 19:08:15 +0300

    feat(auth): auto-login user after signup by creating session

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -3,6 +3,7 @@ import { redirect } from 'next/navigation';
 
 import { hashUserPassword } from '@/lib/hash';
 import { createUser } from '@/lib/user';
+import { createAuthSession } from '@/lib/auth';
 
 export async function signup(prevState, formData) {
   const email = formData.get('email');
@@ -26,17 +27,18 @@ export async function signup(prevState, formData) {
 
   const hashedPassword = hashUserPassword(password);
   try {
-    createUser(email, hashedPassword);
+    const id = createUser(email, hashedPassword);
+    await createAuthSession(id);
+    redirect('/training');
   } catch (error) {
     if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
       return {
         errors: {
-          email: 'It seems like an account for the chosen email already exists.'
-        }
+          email:
+            'It seems like an account for the chosen email already exists.',
+        },
       };
     }
     throw error;
   }
-
-  redirect('/training');
 }
