cfd7cfefa867f09e8a81d13efed471a4ae988347
Vasyl Pryimak <vasyl.pryimak.vasyl.dev@gmail.com>
2026-03-29 19:10:00 +0300

    feat(auth-ui): add login/signup mode toggle to AuthForm

--- components/auth-form.js
+++ components/auth-form.js
@@ -4,7 +4,7 @@ import { useFormState } from 'react-dom';
 
 import { signup } from '@/actions/auth-actions';
 
-export default function AuthForm() {
+export default function AuthForm({ mode }) {
   const [formState, formAction] = useFormState(signup, {});
   return (
     <form id="auth-form" action={formAction}>
@@ -27,10 +27,17 @@ export default function AuthForm() {
         </ul>
       )}
       <p>
-        <button type="submit">Create Account</button>
+        <button type="submit">
+          {mode === 'login' ? 'Login' : 'Create Account'}
+        </button>
       </p>
       <p>
-        <Link href="/">Login with existing account.</Link>
+        {mode === 'login' && (
+          <Link href="/?mode=signup">Create an account.</Link>
+        )}
+        {mode === 'signup' && (
+          <Link href="/?mode=login">Login with existing account.</Link>
+        )}
       </p>
     </form>
   );
